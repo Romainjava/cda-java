e1fbafad1c6e0cadeb3d35b1c8ecec9c6fcc96b7
ROM.VAN_DAM <ROM.VAN_DAM@s124-1.pedago.mermoz.edu>
2019-11-04 14:46:22 +0100

    premier push

--- src/tp2/Tp2Exo1.java
+++ src/tp2/Tp2Exo1.java
@@ -1,9 +1,18 @@
 package tp2;
 
+import java.util.Scanner;
+
 public class Tp2Exo1 {
 
 	public static void main(String[] args) {
-		System.out.print("TP2");
+		
+		Scanner user = new Scanner(System.in);
+		System.out.println("Veuillez rentrez une valeur");
+		int entier_1 = user.nextInt();
+		System.out.println("Veuillez rentrez une valeur");
+		int entier_2 = user.nextInt();
+		
+		System.out.println("Voici la somme des deux valeurs"+ (entier_1 + entier_2) + "et voici le produit"+ (entier_1 * entier_2));
 	}
 
 }
