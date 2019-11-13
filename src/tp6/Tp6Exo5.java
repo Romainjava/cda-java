37e813e72057f81bf8da6f640b57b4683fa53101
Romain <phenixstrato@gmail.com>
2019-11-13 23:25:00 +0100

    exo5 tp6

---
+++ src/tp6/Tp6Exo5.java
@@ -0,0 +1,17 @@
+package tp6;
+
+public class Tp6Exo5 {
+
+	public static void main(String[] args) {
+		char valeur_car ;
+		String valeur_hexa;
+		for (int i = 32; i < 127; i++) {
+			valeur_car = (char)i;
+			valeur_hexa = Integer.toHexString(i);
+			System.out.println(i + " ->  "+ valeur_car + " -> " + valeur_hexa	);
+			
+		}
+
+	}
+
+}
