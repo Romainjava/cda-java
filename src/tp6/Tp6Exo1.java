a260b9190e289f3aaccefef62b4b1ef941e50176
Romain <phenixstrato@gmail.com>
2019-11-13 23:06:30 +0100

    ajout de l'exo 1 TP6

--- src/tp6/Tp6Exo1.java
+++ src/tp6/Tp6Exo1.java
@@ -8,12 +8,10 @@ public class Tp6Exo1 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Rentrez un texte");
 		String user = sc.nextLine();
-		char user_char;
 		int count = 0;
 		
 		for (int i = 0; i < user.length(); i++) {
-			user_char += user.charAt(i);
-			if(user_char == "e") {
+			if(user.charAt(i) == 'e') {
 				count++;
 			}
 		}
